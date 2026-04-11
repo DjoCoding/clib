2d166593afcf15975c99f7c62a44e8b09b7de0ee
DjoCoding <djocoding@gmail.com>
2026-04-11 17:32:42 +0100

    Array macros implemented

---
+++ array.h
@@ -0,0 +1,37 @@
+#ifndef ARRAY_H_
+#define ARRAY_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef ARRAY_INITIAL_SIZE
+#error  "Make sure to define ARRAY_INITIAL_SIZE"
+#endif // ARRAY_INITIAL_SIZE
+
+/**
+ * type Array<T> = {
+ *      T *items;
+ *      size_t len;
+ *      size_t size;
+ */
+
+#define arrappend(array, item) \
+    do { \
+        if((array).len >= (array).size) { \
+            size_t new_size = (array).size * 2; \
+            if(new_size == 0) new_size = ARRAY_INITIAL_SIZE; \
+            \
+            (array).items = realloc((array).items, sizeof(*(array).items) * new_size); \
+            if((array).itesm == NULL) perror("malloc error"); \
+            (array).size = new_size; \
+        } \
+        \
+        (array).items[(array).len - 1] = item; \
+        (array).len += 1; \
+    } while(0)
+
+#define arrclean(array) { (array).len = 0; }
+#define arrfree(array) { free((array).items); (array).len = 0; (array).size = 0; }
+
+#endif
+r
