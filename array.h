a385b1e1ee89db3a3b5acdbaac1b571296d3901a
DjoCoding <djocoding@gmail.com>
2026-04-12 19:33:37 +0100

    Make it possible for the array allocation/deallocation using custom allocators

--- array.h
+++ array.h
@@ -30,7 +30,35 @@
         (array).len += 1; \
     } while(0)
 
+
+/** 
+ * arrappend_allocator: append item to the array using allocator
+ * expect alloc_realloc_func: function(Allocator *allocator, void *ptr, size_t new_size) void *;
+ */
+#define arrappend_allocator(array, item, alloc_realloc_func, allocator) \
+    do { \
+        if((array).len >= (array).size) { \
+            size_t new_size = (array).size * 2; \
+            if(new_size == 0) new_size = ARRAY_INITIAL_SIZE; \
+            \
+            (array).items = alloc_realloc_func(allocator, (array).items, sizeof(*(array).items) * new_size); \
+            if((array).items == NULL) perror("allocator error"); \
+            (array).size = new_size; \
+        } \
+        \
+        (array).items[(array).len] = item; \
+        (array).len += 1; \
+    } while(0)
+
 #define arrclean(array) { (array).len = 0; }
+
 #define arrfree(array) { free((array).items); (array).len = 0; (array).size = 0; }
+
+/** 
+ * arrfree_allocator: free array using allocator
+ * expect alloc_free_func: function(Allocator *allocator, void *ptr) void;
+ */
+#define arrfree_allocator(array, alloc_free_func, allocator) { alloc_free_func(allocator, (array).items); (array).len = 0; (array).size = 0; }
+
 
 #endif
