6eadab003592c793ac1a88253573825bf1c36bc1
DjoCoding <djocoding@gmail.com>
2026-04-14 19:36:15 +0100

    add sb_clone_dup function

--- sb.h
+++ sb.h
@@ -2,6 +2,7 @@
 #define STRING_BUILDER_H_
 
 #include <stdio.h>
+#include <stdint.h>
 
 #ifdef STRING_BUILDER_IMPLEMENTATION
 
@@ -25,9 +26,25 @@ typedef struct {
 StringBuilder *sb_new();
 StringBuilder *sb_new_from_allocator(Allocator *allocator);
 void           sb_reset(StringBuilder *sb);
-void           sb_append(StringBuilder *sb, char *data, size_t len);
+
+void           sb_append_str(StringBuilder *sb, char *data, size_t len);
 void           sb_append_cstr(StringBuilder *sb, const char *cstr);
+
+void           sb_append_i8(StringBuilder *sb,  int8_t v);
+void           sb_append_i16(StringBuilder *sb, int16_t v);
+void           sb_append_i32(StringBuilder *sb, int32_t v);
+void           sb_append_i64(StringBuilder *sb, int64_t v);
+
+void           sb_append_u8(StringBuilder *sb,  uint8_t v);
+void           sb_append_u16(StringBuilder *sb, uint16_t v);
+void           sb_append_u32(StringBuilder *sb, uint32_t v);
+void           sb_append_u64(StringBuilder *sb, uint64_t v);
+
+void           sb_append_float(StringBuilder *sb,  float v);
+void           sb_append_double(StringBuilder *sb, double v);
+
 char          *sb_collect(StringBuilder *sb);
+char          *sb_collect_dup(StringBuilder *sb);
 void           sb_free(StringBuilder *sb);
 
 #ifdef STRING_BUILDER_IMPLEMENTATION__
@@ -35,6 +52,7 @@ void           sb_free(StringBuilder *sb);
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 
 #define __int__sb_max(a, b) ((a) > (b) ? (a) : (b))
 #define __int__sb_min(a, b) ((a) < (b) ? (a) : (b))
@@ -99,20 +117,102 @@ void __int__sb_clamp(StringBuilder *sb, size_t size) {
 }
 
 
-void sb_append(StringBuilder *sb, char *data, size_t len) {
+void sb_append_str(StringBuilder *sb, char *data, size_t len) {
     if(sb->len + len > sb->size) __int__sb_resize(sb, sb->len + len);
     memcpy(sb->data + sb->len, data, len);
     sb->len += len;
 }
 
 void sb_append_cstr(StringBuilder *sb, const char *cstr) {
-    return sb_append(sb, (char *)cstr, strlen(cstr));
+    return sb_append_str(sb, (char *)cstr, strlen(cstr));
+}
+
+void sb_append_i8(StringBuilder *sb,  int8_t v) {
+    char buffer[1024] = {0};
+    snprintf(buffer, 1023, "%" PRIi8, v);
+    sb_append_cstr(sb, buffer);
+}
+
+void sb_append_i16(StringBuilder *sb, int16_t v) {
+    char buffer[1024] = {0};
+    snprintf(buffer, 1023, "%" PRIi16, v);
+    sb_append_cstr(sb, buffer);
+}
+
+void sb_append_i32(StringBuilder *sb, int32_t v) {
+    char buffer[1024] = {0};
+    snprintf(buffer, 1023, "%" PRIi32, v);
+    sb_append_cstr(sb, buffer);
+}
+
+void sb_append_i64(StringBuilder *sb, int64_t v) {
+    char buffer[1024] = {0};
+    snprintf(buffer, 1023, "%" PRIi64, v);
+    sb_append_cstr(sb, buffer);
+}
+
+void sb_append_u8(StringBuilder *sb,  uint8_t v) {
+    char buffer[1024] = {0};
+    snprintf(buffer, 1023, "%" PRIu8, v);
+    sb_append_cstr(sb, buffer);
+}
+
+void sb_append_u16(StringBuilder *sb, uint16_t v) {
+    char buffer[1024] = {0};
+    snprintf(buffer, 1023, "%" PRIu16, v);
+    sb_append_cstr(sb, buffer);
+}
+
+void sb_append_u32(StringBuilder *sb, uint32_t v) {
+    char buffer[1024] = {0};
+    snprintf(buffer, 1023, "%" PRIu32, v);
+    sb_append_cstr(sb, buffer);
+}
+
+void sb_append_u64(StringBuilder *sb, uint64_t v) {
+    char buffer[1024] = {0};
+    snprintf(buffer, 1023, "%" PRIu64, v);
+    sb_append_cstr(sb, buffer);
+}
+
+void sb_append_float(StringBuilder *sb,  float v) {
+    char buffer[1024] = {0};
+    snprintf(buffer, 1023, "%f", v);
+    sb_append_cstr(sb, buffer);
+}
+
+void sb_append_double(StringBuilder *sb, double v) {
+    char buffer[1024] = {0};
+    snprintf(buffer, 1023, "%f", v);
+    sb_append_cstr(sb, buffer);
 }
 
 char *sb_collect(StringBuilder *sb) {
     __int__sb_clamp(sb, sb->len + 1);
     sb->data[sb->len] = 0;    
     return sb->data;
+}
+
+char *__int__sb_alloc(StringBuilder *sb, size_t size) {
+    if(sb->allocator == NULL) {
+        char *ptr = malloc(size);
+        if(ptr == NULL) {
+            perror("malloc failed");
+            abort();
+        }
+        return ptr;
+    }
+    return allocator_alloc(sb->allocator, size);
+} 
+
+char *sb_collect_dup(StringBuilder *sb) {
+    __int__sb_clamp(sb, sb->len + 1);
+    sb->data[sb->len] = 0;
+
+    char *data = __int__sb_alloc(sb, sb->len);  
+    memcpy(data, sb->data, sb->len);
+
+    return data;
 }
 
 void sb_reset(StringBuilder *sb) {
